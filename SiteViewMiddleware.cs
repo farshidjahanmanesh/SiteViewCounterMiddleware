2536d8e819c6344eb1978acdd1bda64b11ab38a1
farshidjahanmanesh <36743160+farshidjahanmanesh@users.noreply.github.com>
2020-05-14 13:02:04 +0430

    Add files via upload

---
+++ SiteViewMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UI.middleware
+{
+    public class SiteViewMiddleware
+    {
+        private static HashSet<string> IpAddress;
+        private static DateTime Date;
+        private RequestDelegate _next;
+        static SiteViewMiddleware()
+        {
+            Date = DateTime.Now;
+            IpAddress = new HashSet<string>();
+        }
+        public SiteViewMiddleware(RequestDelegate _next)
+        {
+            this._next = _next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            await _next(context);
+
+            if ((DateTime.Now - Date).TotalHours > 24)
+            {
+                Date = DateTime.Now;
+                IpAddress.Clear();
+            }
+            var remoteIpAddress = context.Connection.RemoteIpAddress?.ToString();
+            if (remoteIpAddress != null)
+                if (!IpAddress.Any(x => x == remoteIpAddress))
+                {
+                    IpAddress.Add(remoteIpAddress);
+                    //add today date time to database
+                }
+
+
+        }
+
+
+    }
+}
